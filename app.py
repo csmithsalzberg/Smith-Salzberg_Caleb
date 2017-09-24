1703462ba6dc53faf3700bb8196acf33385d0d07
csmithsalzberg <caleb.smithsalzberg@gmail.com>
2017-09-24 12:31:12 -0400

    app

---
+++ app.py
@@ -0,0 +1,29 @@
+#Caleb SMith-Salzberg
+#SoftDev1 pd7
+#HW4 -- Fill Up Yer Flask
+#2017-09-24
+
+from flask import Flask
+app= Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "<b>WASSUPPPP</b>"
+
+@app.route("/cool")
+def pic1():
+    return "<img src= \" https://www.cesarsway.com/sites/newcesarsway/files/styles/large_article_preview/public/Common-dog-behaviors-explained.jpg?itok=FSzwbBoi \" >"
+
+@app.route("/cat")
+def pic2():
+    return "<img src= \"http://r.ddmcdn.com/s_f/o_1/cx_462/cy_245/cw_1349/ch_1349/w_720/APL/uploads/2015/06/caturday-shutterstock_149320799.jpg \" >"
+
+
+@app.route("/motivate")
+
+def motivate():
+    return "<h1>you\'re incredible</h1>"
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
